39bd328e285cd2f726d61e54f37a5c14975d6f8b
Amit Dhakad <dhakadamit@gmail.com>
2013-09-05 17:54:26 +0530

    Rails 4 fix: when using protected_attributes

--- lib/unread/read_mark.rb
+++ lib/unread/read_mark.rb
@@ -1,14 +1,12 @@
 class ReadMark < ActiveRecord::Base
   belongs_to :readable, :polymorphic => true
-  if ActiveRecord::VERSION::MAJOR < 4
-    attr_accessible :readable_id, :user_id, :readable_type, :timestamp
-  end
+  attr_accessible :readable_id, :user_id, :readable_type, :timestamp
 
   validates_presence_of :user_id, :readable_type
 
   scope :global, lambda { where(:readable_id => nil) }
   scope :single, lambda { where('readable_id IS NOT NULL') }
-  scope :older_than, lambda { |timestamp| where([ 'timestamp < ?', timestamp ]) }
+  scope :older_than, lambda { |timestamp| where(['timestamp < ?', timestamp]) }
 
   # Returns the class defined by acts_as_reader
   def self.reader_class
